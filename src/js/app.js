d98ad643b3a962058553478e22ab6a27166de3dc
Nosov Vitaly <vitalinosov2000@gmail.com>
2022-12-27 09:14:51 +0300

    Added and adapt company, activity, benefits and object blocks

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,27 @@
 import * as flsFunctions from "./modules/functions.js"
+import * as animation from "./modules/animation.js"
 
-/*
+
 import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+Swiper.use([Navigation,Pagination]);
+const swiperObject = new Swiper('.swiper-object', {
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    spaceBetween: 9,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-object__button_next',
+        prevEl: '.swiper-object__button_prev',
+    },
+});
+
+const swiperObjectInfo = new Swiper('.swiper-object-info', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-object__button_next',
+        prevEl: '.swiper-object__button_prev',
+    },
+});
